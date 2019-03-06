56ad6c77f665ff2b8008ffbf9dd1c0d6b5074010
visualstudio <visualstudio@122-PC36>
2019-03-06 10:49:55 -0500

    Add project files.

---
+++ CSD_3354_lab_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD_3354_lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
