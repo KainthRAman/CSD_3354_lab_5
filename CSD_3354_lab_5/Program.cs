0cd1b7194e41d527dc7ccb1c1bd4c9d9cc62799b
visualstudio <visualstudio@122-PC36>
2019-03-06 10:57:42 -0500

    Lab05

--- CSD_3354_lab_5/Program.cs
+++ CSD_3354_lab_5/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace CSD_3354_lab_5
 {
-    class Program
-    {
-        static void Main(string[] args)
+   
+            public class Program
         {
+            public static void Main()
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+            }
+        }
+
+        public class DelegateExercises
+        {
+            public delegate int MyDelegate();
+
+            void Method1()
+            {
+                System.Console.WriteLine("MyDelegate");
+            }
+
+            public void Method3()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate();
+            }
         }
     }
-}
+
